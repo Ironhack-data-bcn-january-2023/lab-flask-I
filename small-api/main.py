e96bf93a9bbf98aef2c824fc985acf58589c1a0d
imalanz <arq.imalanz@gmail.com>
2023-02-20 17:48:07 +0100

    Imanol Lab Flaki

--- small-api/main.py
+++ small-api/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import random
+import sql_queries as sql
 
 app = Flask(__name__)
 
@@ -8,5 +9,14 @@ def random_int(therange):
     therange = int(therange)
     return str(random.randint(0, therange)) 
 
+@app.route("/everything-employees")
+def example ():
+    return jsonify(sql.get_everything())
+
+@app.route("/table/<one_table>")
+def table (one_table):
+    return jsonify(sql.table_ten(one_table))
+
 if __name__ == "__main__":
-     app.run(port=7070, debug=False)
+     app.run(port=7070, debug=True)
+
