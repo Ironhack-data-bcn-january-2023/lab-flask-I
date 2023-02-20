a6403de77634aaaf74b6dd8845582542b0b8b424
German Ortola <germanortola@gmail.com>
2023-02-20 17:23:32 +0100

    committing working changes

--- small-api/main.py
+++ small-api/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import random
+import tools.sql_queries as sql
 
 app = Flask(__name__)
 
@@ -13,6 +14,16 @@ def hello_world():
 @app.route("/random-number")
 def random_int():
     return str(random.randint(0, 10))
+
+
+@app.route("/everything-employees")
+def example():
+    return jsonify(sql.get_everything())
+
+@app.route("/table/<one_table>")
+def example2(one_table):
+    return jsonify(sql.table_ten(one_table))
+
 
 
 if __name__ == "__main__":
