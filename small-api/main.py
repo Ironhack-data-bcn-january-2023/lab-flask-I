e0ff036e56312b49d1d48a78e898f31356ed3193
Gonzalo Quesada <gqsalvat@gmail.com>
2023-02-20 16:11:25 +0100

    Commit at: 02/20/23 16:11:25

---
+++ small-api/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+import random
+
+@app.route("/")
+def hello_world():
+    return "Hello world!"
+
+@app.route("/random-number")
+def random_number ():
+   
+    return str(random.randint(0, 10))
+
+if __name__ == "__main__":
+     app.run(port=7070, debug=False)
