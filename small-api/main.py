0886dcd284a552a1aa03436696fd05548375c6e3
imalanz <arq.imalanz@gmail.com>
2023-02-21 17:49:48 +0100

    Imanol lab Flask2

--- small-api/main.py
+++ small-api/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import random
 import sql_queries as sql
+import requests
 
 app = Flask(__name__)
 
@@ -16,6 +17,34 @@ def example ():
 @app.route("/table/<one_table>")
 def table (one_table):
     return jsonify(sql.table_ten(one_table))
+
+# POST THINGS INTO SQL
+@app.route("/insert/<the_table>/<d_id>/<name>", methods = ["POST"])
+def insert_into_table (the_table, d_id, name):
+    sql.insert_one(the_table, d_id, name)
+    return "Inserted!"
+
+@app.route("/insert-into-employees", methods=["POST"])
+def insert_by_passing_params ():
+
+    emp_no = request.args["emp_no"] 
+    birth_date = request.args["birth_date"]
+    first_name = request.args["first_name"] 
+    last_name = request.args["last_name"] 
+    gender = request.args["gender"] 
+    hire_date = request.args["hire_date"] 
+
+    sql.insert_params (emp_no, birth_date, first_name, last_name, gender, hire_date)
+    return "ok"
+
+@app.route("/insert-department", methods=["POST"])
+def insert_depart ():
+
+    dept_no = request.args["dept_no"] 
+    dept_name = request.args["dept_name"]
+
+    sql.insert_dept (dept_no, dept_name)
+    return "ok"
 
 if __name__ == "__main__":
      app.run(port=7070, debug=True)
--- small-api/sql_queries.py
+++ small-api/sql_queries.py
@@ -10,3 +10,17 @@ def table_ten(one_table):
     query = f"SELECT * FROM {one_table}  LIMIT 10;"
     df = pd.read_sql_query(query, engine)
     return df.to_dict(orient="records")
+
+def insert_params (emp_no, birth_date, first_name, last_name, gender, hire_date):
+    query = f"""
+    INSERT INTO employees
+    VALUES ("{emp_no}", "{birth_date}", "{first_name}", "{last_name}", "{gender}", "{hire_date}");
+    """
+    engine.execute(query)
+
+def insert_dept (dept_no, dept_name):
+    query = f"""
+    INSERT INTO departments
+    VALUES ("{dept_no}", "{dept_name}");
+    """
+    engine.execute(query)
